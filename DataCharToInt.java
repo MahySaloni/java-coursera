d43a31f6dc5292b072afcd8f6aa6799d5cffdb6c
Saloni Mahyavanshi <smahyavanshi@binghamton.edu>
2025-06-12 18:52:07 +0400

    6/12/2025

---
+++ DataCharToInt.java
@@ -0,0 +1,8 @@
+public class DataCharToInt {
+    public static void main(String[] args) {
+        char a = 'a';
+        System.out.println("a is " + a);
+        int asciiValue = (int) a;
+        System.out.println("ASCII value of 'a' is " + asciiValue);
+    }
+}
